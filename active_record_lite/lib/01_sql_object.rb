86fa8f59ae957674dbfe6e53f9c98adeb70ce7c9
Joseph-Kung <joseph.kung@hotmail.com>
2018-11-09 14:08:27 -0800

    Create #initialize, ::all, and :parse_all

--- active_record_lite/lib/01_sql_object.rb
+++ active_record_lite/lib/01_sql_object.rb
@@ -6,14 +6,16 @@ require 'byebug'
 
 class SQLObject
   def self.columns
-    @columns ||= DBConnection.execute2(<<-SQL)
+    return @columns if @columns
+    @columns = DBConnection.execute2(<<-SQL)
               SELECT
                 *
               FROM
-              '#{self}s'
+              '#{self.table_name}'
             SQL
 
     @columns = @columns.first.map(&:to_sym) unless @columns.first.is_a?(Symbol)
+    return @columns
   end
 
   def self.finalize!
@@ -37,11 +39,16 @@ class SQLObject
   end
 
   def self.all
-    # ...
+    table = DBConnection.execute(<<-SQL)
+              SELECT *
+              FROM "#{self.table_name}"
+            SQL
+
+    self.parse_all(table)
   end
 
   def self.parse_all(results)
-    # ...
+    results.map { |row| self.new(row) }
   end
 
   def self.find(id)
@@ -49,7 +56,15 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |param, value|
+      param = param.to_sym
+
+      if self.class.columns.include?(param)
+        self.send("#{param}=", value)
+      else
+        raise "unknown attribute '#{param}'"
+      end
+    end
   end
 
   def attributes
