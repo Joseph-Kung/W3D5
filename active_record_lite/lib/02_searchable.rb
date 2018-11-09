620a5ae89034c3b0033a5efb4989a982f4bfacf2
Joseph-Kung <joseph.kung@hotmail.com>
2018-11-09 15:02:58 -0800

    Write Searchable module and #where method to return an object that match parameters

--- active_record_lite/lib/02_searchable.rb
+++ active_record_lite/lib/02_searchable.rb
@@ -3,10 +3,20 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_line = params.keys.map { |key| "#{key} = ?" }.join(' AND ')
+
+    query = DBConnection.execute(<<-SQL, *params.values)
+              SELECT *
+              FROM #{self.table_name}
+              WHERE #{where_line}
+            SQL
+
+    query.map { |result| self.new(result) }
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
+
+
 end
